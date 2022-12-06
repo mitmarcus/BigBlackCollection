dbfa9b21ab9ad35c94cbb033563aab8538bfec24
ricardofernandes21 <114725680+ricardofernandes21@users.noreply.github.com>
2022-12-06 13:45:32 +0100

    asdasasddas

--- src/utils/File.java
+++ src/utils/File.java
@@ -2,14 +2,14 @@ package utils;
 
 import java.io.PrintWriter;
 
-public class File {
-<<<<<<< Updated upstream
-=======
+public class File
+{
+
 	/*File file = new File(filename);
 	PrintWriter out = new PrintWriter(file);
 
 	String xml = "";
 	xml += "<?xml version=\"1.0\" encoding=\"UTF-8\"" + "standalone=\"no\"?>\n";*/
->>>>>>> Stashed changes
 }
+
 
