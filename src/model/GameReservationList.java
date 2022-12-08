685c104882dca4d720becf401c01412d6ba5a07b
Mark <markg546@gmail.com>
2022-12-08 13:47:57 +0100

    shit

--- src/model/GameReservationList.java
+++ src/model/GameReservationList.java
@@ -3,63 +3,76 @@ import java.util.ArrayList;
 //mark
 
 public class GameReservationList {
-    private Game game;
-    private User user;
     private ArrayList<GameReservation> reservations;
-    private MyDate date;
-    public GameReservationList(Game game) {
-        this.game = game;
-        reservations = new ArrayList<GameReservation>();
-    }
-    public GameReservation getGameReservation(Game game) {
-        for (GameReservation reservation : reservations) {
-            if (reservation.getGame() == game) {
-                return reservation;
-            }
-        }
-        return null;
-    }
 
-    public GameReservation getGameReservationAtPosition(int index) {
-        return reservations.get(index);
-    }
-
-    public MyDate getBorrowDateForAGame(Game game) {
-        for (GameReservation reservation : reservations) {
-            if (reservation.getGame() == game) {
-                return reservation.getFromDate();
-            }
-        }
-        return null;
-    }
-
-    public MyDate getReturnDateForAGame(Game game) {
-        for (GameReservation reservation : reservations) {
-            if (reservation.getGame() == game) {
-                return reservation.getToDate();
-            }
-        }
-        return null;
-    }
-
-    public User getGameLentTo(Game game) {
-        for (GameReservation reservation : reservations) {
-            if (reservation.getGame() == game) {
-                return reservation.lentTo();
-            }
-        }
-        return null;
-    }
-
-    public void addGameReservation(GameReservation reservation) {
+    public void addReservation(GameReservation reservation) {
         reservations.add(reservation);
     }
 
-    public void removeGameReservation(GameReservation reservation) {
-        reservations.remove(reservation);
+    public boolean removeReservation(GameReservation reservation) {
+        return reservations.remove(reservation);
     }
 
-    public void editGameReservation(GameReservation reservation, Game game, MyDate fromDate, MyDate toDate, User user) {
-        reservation.set(game, fromDate, toDate, user);
+    public ArrayList<GameReservation> getReservations() {
+        return reservations;
     }
 }
+
+//    private Game game;
+//    private User user;
+//    private MyDate date;
+//    public GameReservationList(Game game) {
+//        reservations = new ArrayList<GameReservation>();
+//    }
+//    public GameReservation getGameReservation(Game game) {
+//        for (GameReservation reservation : reservations) {
+//            if (reservation.getGame() == game) {
+//                return reservation;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public GameReservation getGameReservationAtPosition(int index) {
+//        return reservations.get(index);
+//    }
+//
+//    public MyDate getBorrowDateForAGame(Game game) {
+//        for (GameReservation reservation : reservations) {
+//            if (reservation.getGame() == game) {
+//                return reservation.getFromDate();
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public MyDate getReturnDateForAGame(Game game) {
+//        for (GameReservation reservation : reservations) {
+//            if (reservation.getGame() == game) {
+//                return reservation.getToDate();
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public User getGameLentTo(Game game) {
+//        for (GameReservation reservation : reservations) {
+//            if (reservation.getGame() == game) {
+//                return reservation.lentTo();
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public void addGameReservation(GameReservation reservation) {
+//        reservations.add(reservation);
+//    }
+//
+//    public void removeGameReservation(GameReservation reservation) {
+//        reservations.remove(reservation);
+//    }
+//
+//    public void editGameReservation(GameReservation reservation, Game game, MyDate fromDate, MyDate toDate, User user) {
+//        reservation.set(game, fromDate, toDate, user);
+//    }
+//}
---
+++ src/view/ReservationGUI.java
@@ -0,0 +1,98 @@
+//package view;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.*;
+//import javafx.scene.layout.Region;
+//import model.BBCmodel;
+//import model.Game;
+//import model.GameReservation;
+//import model.User;
+//
+//public class ReservationGUI {
+//    public ReservationGUI() {
+//        // Called by FXMLLoader
+//    }
+//
+////    public void init(ViewHandler viewHandler, BBCmodel model, Region root) {
+////        this.model = model;
+////        this.viewHandler = viewHandler;
+////        this.root = root;
+////
+////
+////        reset();
+////    }
+//
+////    public void initialize()
+////    {
+////        ????
+////        modelManager = new ModelManager("reservations.bin");
+////
+////        updateGamesForReservationBox();
+////        updateReservationBox();
+////
+////    }
+//
+//    public Region getRoot() {
+//        return root;
+//    }
+//
+//    public void reset() {
+//    }
+//
+//    @FXML
+//    private void goBack() {
+//        viewHandler.openView("main");
+//    }
+//
+//
+//    @FXML
+//    private Tab reservationsTab;
+//    @FXML
+//    private TabPane reservationsTabPane;
+//
+//    // addReservation
+//    @FXML
+//    private Tab addReservationTab;
+//    @FXML
+//    private ComboBox<User> addReservationMember;
+//    @FXML
+//    private ComboBox<Game> addReservationGame;
+//    @FXML
+//    private DatePicker addReservationSDate;
+//    @FXML
+//    private TextField addReservationSHour;
+//    @FXML
+//    private TextField addReservationSMinute;
+//    @FXML
+//    private DatePicker addReservationEDate;
+//    @FXML
+//    private TextField addReservationEHour;
+//    @FXML
+//    private TextField addReservationEMinute;
+//    @FXML
+//    private Button addReservationConfirm;
+//
+//
+//    // manageReservations
+//    @FXML
+//    private Tab manageReservationsTab;
+//    @FXML
+//    private ComboBox<GameReservation> reservationsListTab;
+//    @FXML
+//    private Label manageReservationsMember;
+//    @FXML
+//    private Label manageReservationsGame;
+//    @FXML
+//    private Label manageReservationsSDate;
+//    @FXML
+//    private Label manageReservationsEDate;
+//    @FXML
+//    private Button manageReservationsEnd;
+//    @FXML
+//    private Button manageReservationsToBorrowing;
+//    @FXML
+//    private TextField manageReservationsLocation;
+//
+//
+//}
+//
